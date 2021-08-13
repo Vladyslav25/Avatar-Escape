0f9e606e5bc2f195397b956f2af0e7ad8a990f57
Tim Velling <tim.velling@hotmail.de>
2021-08-13 23:44:37 +0200

    Circular Motion

--- Assets/HandMovement.cs
+++ Assets/HandMovement.cs
@@ -7,20 +7,30 @@ public class HandMovement : MonoBehaviour
 {
     [SerializeField] private AnimationCurve m_handCurve;
     [SerializeField] private float m_speed = 0.2f;
-    [SerializeField] private Vector2 m_scale = Vector2.one;
+    //[SerializeField] private Vector2 m_scale = Vector2.one;
     private VisualEffect m_effect;
     private float m_time;
+    private int m_effectWaveId;
+    private Rigidbody m_rig;
 
     private void Start()
     {
         m_effect = GetComponentInChildren<VisualEffect>();
+        m_effectWaveId = Shader.PropertyToID("SpawnPos");
+        m_rig = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        m_time = (m_time + Time.deltaTime * m_speed) % 1f;
-        m_effect.SetVector3("SpawnPos", this.transform.position);
-        transform.position = new Vector3(m_handCurve.Evaluate(m_time) * m_scale.x, 0, m_time * m_scale.y);
+        m_time = (m_time + Time.fixedDeltaTime * m_speed) % 1f;
+        m_effect.SetVector3(m_effectWaveId, this.transform.position);
+        //m_rig.MovePosition(new Vector3(m_handCurve.Evaluate(m_time) * m_scale.x, 0, m_time * m_scale.y));
+
+        Vector3 distance = Vector3.zero - transform.position;
+        Vector3 normalizedDistance = distance.normalized;
+
+        Vector3 perpendicularDistance = new Vector3(-normalizedDistance.z, 0, normalizedDistance.x);
+
+        m_rig.velocity = perpendicularDistance * m_speed;
     }
 }
