dc56fcc022cff91939282bbb16a6408fe6d6f01d
Tim Velling <tim.velling@hotmail.de>
2021-08-14 13:43:19 +0200

    More idepentent effect

--- Assets/Scripts/ExampleAirPower.cs
+++ Assets/Scripts/ExampleAirPower.cs
@@ -6,15 +6,25 @@ using UnityEngine.VFX;
 public class ExampleAirPower : MonoBehaviour
 {
     [SerializeField, Range(0.1f, 1)] private float m_chragedPower;
+    [SerializeField] private bool m_isActiveCharging;
     [SerializeField] private VisualEffect m_effect;
-    int m_effectWaveId;
+    private int m_isActive;
+    private int m_effectWaveId;
     private void Start()
     {
         m_effectWaveId = Shader.PropertyToID("ChargedPower");
+        m_isActive = Shader.PropertyToID("KeepSpawning");
     }
 
     private void Update()
     {
         m_effect.SetFloat(m_effectWaveId, m_chragedPower);
+        ChangeActive = m_isActiveCharging;
+    }
+
+    public bool ChangeActive
+    {
+        get => m_effect.GetBool(m_isActive);
+        set => m_effect.SetBool(m_isActive, value);
     }
 }
