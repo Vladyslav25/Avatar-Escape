a8411b76c31b0d82672dfab982e55f78174a79eb
Lucas Ciba <lucasciba@online.de>
2021-08-15 02:09:13 +0200

    Wwise added Events Enemies Voices

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,49 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_ENEMY_APPEARS_JAP = 511960625U;
+        static const AkUniqueID PLAY_ENEMY_APPEARS_KISS = 3218283006U;
+        static const AkUniqueID PLAY_ENEMY_APPEARS_NORMAL = 1637247213U;
+        static const AkUniqueID PLAY_ENEMY_APPEARS_PIG = 4198696536U;
+        static const AkUniqueID PLAY_ENEMY_APPEARS_SPAN = 2108511140U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID ENEMIES = 2242381963U;
+        static const AkUniqueID ENVIRONMENT = 1229948536U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID PLAYER = 1069431850U;
+        static const AkUniqueID SHIP = 284967655U;
+        static const AkUniqueID UI = 1551306167U;
+        static const AkUniqueID WIND = 1537061107U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
