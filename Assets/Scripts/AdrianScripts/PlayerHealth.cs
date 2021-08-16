4281c427f2a79ffaef7e11f314f61ad0f9d97186
GreenGift <aschleeh@gmx.de>
2021-08-16 17:36:23 +0200

    fixed some bugs

--- Assets/Scripts/AdrianScripts/PlayerHealth.cs
+++ Assets/Scripts/AdrianScripts/PlayerHealth.cs
@@ -12,18 +12,63 @@ public class PlayerHealth : MonoBehaviour
     [SerializeField]
     private Flagswitch m_LifeFlag;
 
+    [SerializeField]
+    private AK.Wwise.Event m_FullLifeEvent;
+
+    //[SerializeField]
+    //private AK.Wwise.Event m_StopNormalLife;
+    [SerializeField]
+    private AK.Wwise.Event m_midLifeEvent;
+
+    //[SerializeField]
+    //private AK.Wwise.Event m_StopMidLide;
+    [SerializeField]
+    private AK.Wwise.Event m_lowLifeEvent;
+
     private void Awake()
     {
         m_currentHealth = m_maxHealth;
+        _ = m_FullLifeEvent.Post(this.gameObject);
+    }
+
+    [ContextMenu("DamageBoat")]
+    private void DamageDebug()
+    {
+        BoatGotDamage(1);
     }
 
     public void BoatGotDamage(int amount)
     {
+
+        int newLife = m_currentHealth - amount;
+
+        if (newLife > 2 && newLife <= 4)
+        {
+            if (m_currentHealth > 4)
+            {
+                Debug.Log("MId");
+                //_ = m_StopNormalLife.Post(this.gameObject);
+                // _ = m_midLifeEvent.Post(this.gameObject);
+            }
+        }
+
+        else if (newLife > 0 && newLife <= 2)
+        {
+            if (m_currentHealth > 2)
+            {
+                Debug.Log("LOw");
+                //_ = m_StopMidLide.Post(this.gameObject);
+                //_ = m_lowLifeEvent.Post(this.gameObject);
+            }
+        }
+
         m_currentHealth -= amount;
 
         m_currentHealth = m_currentHealth < 0 ? 0 : m_currentHealth;
 
         m_LifeFlag.HP = m_currentHealth;
+
+
 
         CheckForLose();
     }
